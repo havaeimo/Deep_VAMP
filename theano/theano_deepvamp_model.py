07296f2c3a153ef60176e8ebc6db8facd5fcb468
Mohammad Havaei <seyed.mohammad.havaei@usherbrooke.ca>
2015-05-27 16:41:53 -0400

    debugging

--- theano/theano_deepvamp_model.py
+++ theano/theano_deepvamp_model.py
@@ -1,21 +1,25 @@
 import models
-import deepvamp_dataset
-
+from deep_vamp import VAMP
+import argparse
+import pdb
+import os
+import theano.tensor as T
+import numpy as np
 class DeepVamp(object):
 
-    def __init__(self, dataset, random_seed, hiddensize=[500,100]):
-    	input = T.matrix('input')   # the data is presented as rasterized images
+    def __init__(self, input, random_seed, hiddensize=[500,100]):
+    	  # the data is presented as rasterized images
     	target = T.matrix('target')  # the labels are presented as 1D vector of
 
-    	input_size = output_size = dataset['input_size']
+    	#input_size = output_size = dataset['input_size']
     	self.rng = np.random.mtrand.RandomState(random_seed)
 
-    	self.layers = [medels.LeNetConvPoolLayer(self.rng, input, filter_shape=(?,?), image_shape=(?,?), activation=None, pool_size=(?, ?), pool_stride=(?,?))]
+    	self.layers = [models.LeNetConvPoolLayer(self.rng, input, filter_shape=(3,3,5,5), image_shape=(100,3,32,32), activation=None, pool_size=(4, 4), pool_stride=(1,1))]
 
-    	self.layers += models.OutputLayer(input=self.layers[-1].out, filter_shape=(?,?), image_shape=(?,?),n_classes=?)
+    	self.layers += models.OutputLayer(input=self.layers[-1].out, filter_shape=(3,2,21,21), image_shape=(21,21),n_classes=2)
 
     	cost_obj = models.Cost(self.layers[-1].out, target)
-    	cost = cost_obj.out
+    	self.cost = cost_obj.out
 
         layer_parameters = []
         for i in range(2):
@@ -29,7 +33,7 @@ class DeepVamp(object):
 
         #self.train = theano.function([input],cost, updates=updates)    
 
-
+'''
         self.train_model = theano.function(
         [index],
         cost,
@@ -40,25 +44,26 @@ class DeepVamp(object):
         }
     )
 
-
+    
         index = T.lscalar()
         self.learn = theano.function(name='learn',
                                      inputs=[index],
                                      outputs=loss,
                                      updates=updates,
                                      givens={input: dataset['train']['data'][index * batch_size:(index + 1) * batch_size], target: dataset['train']['target'][index * batch_size:(index + 1) * batch_size]}) 
-
+    
 
 
         self.use = theano.function(name='use',
                                    inputs=[input],
-                                   outputs=output) 
+                                   outputs=cost) 
+    '''
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description='Generate the DICE score for a BrainSet')
-    parser.add_argument('model', type=argparse.FileType('r'),
-                        help='A serialized pylearn2 model.')
+    #parser = argparse.ArgumentParser(description='Generate the DICE score for a BrainSet')
+    #parser.add_argument('model', type=argparse.FileType('r'),
+     #                   help='A serialized pylearn2 model.')
     #####parser.add_argument('testset', type=str,
     #                    help='The path to test images.'),
     #parser.add_argument('patch_shape', type=int,
@@ -67,7 +72,26 @@ if __name__ == "__main__":
     #                    help='The size of the predicted patch window.'),
     #parser.add_argument('num_channels', type=int,
     #                    help='Number of channels in the dataset.'),
-    args = parser.parse_args()
+    #args = parser.parse_args()
     #path_testset = self.testset
+    x = T.matrix('x') 
     path_testset = '/home/local/USHERBROOKE/havm2701/data/DBFrames'
+    batch_size = 100
+    train = VAMP()
+    train_set_x = train.X[:100,:].reshape(100,3,32,32)
 
+    train_set_y = train.y[:100,:]
+
+    classifier = DeepVamp(input=x,random_seed=1234)
+    pdb.set_trace()
+
+    test_model = theano.function(
+    inputs=[index],
+    outputs=classifier.layers[-1].out,
+    givens={
+        x: test_set_x[index * batch_size:(index + 1) * batch_size],
+        #y: test_set_y[index * batch_size:(index + 1) * batch_size]
+    }
+    )
+
+    test_model(0)
