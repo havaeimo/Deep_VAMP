e6a95c07b5da265b7b4423e3801dc996e49e6e58
Mohammad Havaei <seyed.mohammad.havaei@usherbrooke.ca>
2015-06-01 20:38:16 -0400

    structured the new script and made it ready for various input sizes

--- theano/mnist_test.py
+++ theano/mnist_test.py
@@ -1,7 +1,7 @@
 from theano.tensor.signal import downsample
 from theano import tensor as T
 from theano.tensor.nnet import conv
-#import models
+import models2
 import cPickle
 import gzip
 import os
@@ -13,384 +13,24 @@ from deep_vamp import VAMP
 import theano
 import theano.tensor as T
 
-class HiddenLayer(object):
-    def __init__(self, rng, input, n_in, n_out, W=None, b=None,
-                 activation=T.tanh):
-        """
-        Typical hidden layer of a MLP: units are fully-connected and have
-        sigmoidal activation function. Weight matrix W is of shape (n_in,n_out)
-        and the bias vector b is of shape (n_out,).
-
-        NOTE : The nonlinearity used here is tanh
-
-        Hidden unit activation is given by: tanh(dot(input,W) + b)
-
-        :type rng: numpy.random.RandomState
-        :param rng: a random number generator used to initialize weights
-
-        :type input: theano.tensor.dmatrix
-        :param input: a symbolic tensor of shape (n_examples, n_in)
-
-        :type n_in: int
-        :param n_in: dimensionality of input
-
-        :type n_out: int
-        :param n_out: number of hidden units
-
-        :type activation: theano.Op or function
-        :param activation: Non linearity to be applied in the hidden
-                           layer
-        """
-        self.input = input
-        # end-snippet-1
-
-        # `W` is initialized with `W_values` which is uniformely sampled
-        # from sqrt(-6./(n_in+n_hidden)) and sqrt(6./(n_in+n_hidden))
-        # for tanh activation function
-        # the output of uniform if converted using asarray to dtype
-        # theano.config.floatX so that the code is runable on GPU
-        # Note : optimal initialization of weights is dependent on the
-        #        activation function used (among other things).
-        #        For example, results presented in [Xavier10] suggest that you
-        #        should use 4 times larger initial weights for sigmoid
-        #        compared to tanh
-        #        We have no info for other function, so we use the same as
-        #        tanh.
-        if W is None:
-            W_values = numpy.asarray(
-                rng.uniform(
-                    low=-numpy.sqrt(6. / (n_in + n_out)),
-                    high=numpy.sqrt(6. / (n_in + n_out)),
-                    size=(n_in, n_out)
-                ),
-                dtype=theano.config.floatX
-            )
-            if activation == theano.tensor.nnet.sigmoid:
-                W_values *= 4
-
-            W = theano.shared(value=W_values, name='W', borrow=True)
-
-        if b is None:
-            b_values = numpy.zeros((n_out,), dtype=theano.config.floatX)
-            b = theano.shared(value=b_values, name='b', borrow=True)
-
-        self.W = W
-        self.b = b
-
-        lin_output = T.dot(input, self.W) + self.b
-        self.output = (
-            lin_output if activation is None
-            else activation(lin_output)
-        )
-        # parameters of the model
-        self.params = [self.W, self.b]
-
-
-class LogisticRegression(object):
-    """Multi-class Logistic Regression Class
-
-    The logistic regression is fully described by a weight matrix :math:`W`
-    and bias vector :math:`b`. Classification is done by projecting data
-    points onto a set of hyperplanes, the distance to which is used to
-    determine a class membership probability.
-    """
-
-    def __init__(self, input, n_in, n_out):
-        """ Initialize the parameters of the logistic regression
-
-        :type input: theano.tensor.TensorType
-        :param input: symbolic variable that describes the input of the
-                      architecture (one minibatch)
-
-        :type n_in: int
-        :param n_in: number of input units, the dimension of the space in
-                     which the datapoints lie
-
-        :type n_out: int
-        :param n_out: number of output units, the dimension of the space in
-                      which the labels lie
-
-        """
-        # start-snippet-1
-        # initialize with 0 the weights W as a matrix of shape (n_in, n_out)
-        self.W = theano.shared(
-            value=numpy.zeros(
-                (n_in, n_out),
-                dtype=theano.config.floatX
-            ),
-            name='W',
-            borrow=True
-        )
-        # initialize the baises b as a vector of n_out 0s
-        self.b = theano.shared(
-            value=numpy.zeros(
-                (n_out,),
-                dtype=theano.config.floatX
-            ),
-            name='b',
-            borrow=True
-        )
-
-        # symbolic expression for computing the matrix of class-membership
-        # probabilities
-        # Where:
-        # W is a matrix where column-k represent the separation hyper plain for
-        # class-k
-        # x is a matrix where row-j  represents input training sample-j
-        # b is a vector where element-k represent the free parameter of hyper
-        # plain-k
-        self.p_y_given_x = T.nnet.softmax(T.dot(input, self.W) + self.b)
-
-        # symbolic description of how to compute prediction as class whose
-        # probability is maximal
-        self.y_pred = T.argmax(self.p_y_given_x, axis=1)
-        # end-snippet-1
-
-        # parameters of the model
-        self.params = [self.W, self.b]
-
-    def negative_log_likelihood(self, y):
-        """Return the mean of the negative log-likelihood of the prediction
-        of this model under a given target distribution.
-
-        .. math::
-
-            \frac{1}{|\mathcal{D}|} \mathcal{L} (\theta=\{W,b\}, \mathcal{D}) =
-            \frac{1}{|\mathcal{D}|} \sum_{i=0}^{|\mathcal{D}|}
-                \log(P(Y=y^{(i)}|x^{(i)}, W,b)) \\
-            \ell (\theta=\{W,b\}, \mathcal{D})
-
-        :type y: theano.tensor.TensorType
-        :param y: corresponds to a vector that gives for each example the
-                  correct label
-
-        Note: we use the mean instead of the sum so that
-              the learning rate is less dependent on the batch size
-        """
-        # start-snippet-2
-        # y.shape[0] is (symbolically) the number of rows in y, i.e.,
-        # number of examples (call it n) in the minibatch
-        # T.arange(y.shape[0]) is a symbolic vector which will contain
-        # [0,1,2,... n-1] T.log(self.p_y_given_x) is a matrix of
-        # Log-Probabilities (call it LP) with one row per example and
-        # one column per class LP[T.arange(y.shape[0]),y] is a vector
-        # v containing [LP[0,y[0]], LP[1,y[1]], LP[2,y[2]], ...,
-        # LP[n-1,y[n-1]]] and T.mean(LP[T.arange(y.shape[0]),y]) is
-        # the mean (across minibatch examples) of the elements in v,
-        # i.e., the mean log-likelihood across the minibatch.
-        return -T.mean(T.log(self.p_y_given_x)[T.arange(y.shape[0]), y])
-        # end-snippet-2
-
-    def errors(self, y):
-        """Return a float representing the number of errors in the minibatch
-        over the total number of examples of the minibatch ; zero one
-        loss over the size of the minibatch
-
-        :type y: theano.tensor.TensorType
-        :param y: corresponds to a vector that gives for each example the
-                  correct label
-        """
-
-        # check if y has same dimension of y_pred
-        if y.ndim != self.y_pred.ndim:
-            raise TypeError(
-                'y should have the same shape as self.y_pred',
-                ('y', y.type, 'y_pred', self.y_pred.type)
-            )
-        # check if y is of the correct datatype
-        if y.dtype.startswith('int'):
-            # the T.neq operator returns a vector of 0s and 1s, where 1
-            # represents a mistake in prediction
-            return T.mean(T.neq(self.y_pred, y))
-        else:
-            raise NotImplementedError()
-
-def load_data(dataset):
-    ''' Loads the dataset
-
-    :type dataset: string
-    :param dataset: the path to the dataset (here MNIST)
-    '''
-
-    #############
-    # LOAD DATA #
-    #############
-
-    # Download the MNIST dataset if it is not present
-    data_dir, data_file = os.path.split(dataset)
-    if data_dir == "" and not os.path.isfile(dataset):
-        # Check if dataset is in the data directory.
-        new_path = os.path.join(
-            os.path.split(__file__)[0],
-            "..",
-            "data",
-            dataset
-        )
-        if os.path.isfile(new_path) or data_file == 'mnist.pkl.gz':
-            dataset = new_path
-
-    if (not os.path.isfile(dataset)) and data_file == 'mnist.pkl.gz':
-        import urllib
-        origin = (
-            'http://www.iro.umontreal.ca/~lisa/deep/data/mnist/mnist.pkl.gz'
-        )
-        print 'Downloading data from %s' % origin
-        urllib.urlretrieve(origin, dataset)
-
-    print '... loading data'
-
-    # Load the dataset
-    f = gzip.open(dataset, 'rb')
-    train_set, valid_set, test_set = cPickle.load(f)
-    f.close()
-    #train_set, valid_set, test_set format: tuple(input, target)
-    #input is an numpy.ndarray of 2 dimensions (a matrix)
-    #witch row's correspond to an example. target is a
-    #numpy.ndarray of 1 dimensions (vector)) that have the same length as
-    #the number of rows in the input. It should give the target
-    #target to the example with the same index in the input.
-
-    def shared_dataset(data_xy, borrow=True):
-        """ Function that loads the dataset into shared variables
-
-        The reason we store our dataset in shared variables is to allow
-        Theano to copy it into the GPU memory (when code is run on GPU).
-        Since copying data into the GPU is slow, copying a minibatch everytime
-        is needed (the default behaviour if the data is not in a shared
-        variable) would lead to a large decrease in performance.
-        """
-        data_x, data_y = data_xy
-        shared_x = theano.shared(numpy.asarray(data_x,
-                                               dtype=theano.config.floatX),
-                                 borrow=borrow)
-        shared_y = theano.shared(numpy.asarray(data_y,
-                                               dtype=theano.config.floatX),
-                                 borrow=borrow)
-        # When storing data on the GPU it has to be stored as floats
-        # therefore we will store the labels as ``floatX`` as well
-        # (``shared_y`` does exactly that). But during our computations
-        # we need them as ints (we use labels as index, and if they are
-        # floats it doesn't make sense) therefore instead of returning
-        # ``shared_y`` we will have to cast it to int. This little hack
-        # lets ous get around this issue
-        return shared_x, T.cast(shared_y, 'int32')
-
-    test_set_x, test_set_y = shared_dataset(test_set)
-    valid_set_x, valid_set_y = shared_dataset(valid_set)
-    train_set_x, train_set_y = shared_dataset(train_set)
-
-    rval = [(train_set_x, train_set_y), (valid_set_x, valid_set_y),
-            (test_set_x, test_set_y)]
-    return rval
-
-
-
-class LeNetConvPoolLayer(object):
-    """Pool Layer of a convolutional network """
-
-    def __init__(self, rng, input, filter_shape, image_shape, activation=T.tanh, pool_size=(2, 2),conv_stride=(1,1),pool_stride=None):
-
-        """
-        Allocate a LeNetConvPoolLayer with shared variable internal parameters.
-
-        :type rng: numpy.random.RandomState
-        :param rng: a random number generator used to initialize weights
-
-        :type input: theano.tensor.dtensor4
-        :param input: symbolic image tensor, of shape image_shape
-
-        :type filter_shape: tuple or list of length 4
-        :param filter_shape: (number of filters, num input feature maps,
-                              filter height, filter width)
-
-        :type image_shape: tuple or list of length 4
-        :param image_shape: (batch size, num input feature maps,
-                             image height, image width)
-
-        :type poolsize: tuple or list of length 2
-        :param poolsize: the downsampling (pooling) factor (#rows, #cols)
-        """
-
-        assert image_shape[1] == filter_shape[1]
-        self.input = input
-
-        # there are "num input feature maps * filter height * filter width"
-        # inputs to each hidden unit
-        fan_in = numpy.prod(filter_shape[1:])
-        # each unit in the lower layer receives a gradient from:
-        # "num output feature maps * filter height * filter width" /
-        #   pooling size
-        fan_out = (filter_shape[0] * numpy.prod(filter_shape[2:]) /
-                   numpy.prod(pool_size))
-        # initialize weights with random weights
-        W_bound = numpy.sqrt(6. / (fan_in + fan_out))
-        self.W = theano.shared(
-            numpy.asarray(
-                rng.uniform(low=-W_bound, high=W_bound, size=filter_shape),
-                dtype=theano.config.floatX
-            ),
-            borrow=True
-        )
-
-        # the bias is a 1D tensor -- one bias per output feature map
-        b_values = numpy.zeros((filter_shape[0],), dtype=theano.config.floatX)
-        self.b = theano.shared(value=b_values, borrow=True)
-
-        # convolve input feature maps with filters
-        conv_out = conv.conv2d(
-            input=input,
-            filters=self.W,
-            filter_shape=filter_shape,
-            image_shape=image_shape
-        )
-
-        # downsample each feature map individually, using maxpooling
-        pooled_out = downsample.max_pool_2d(
-            input=conv_out,
-            ds=pool_size,
-            ignore_border=True
-        )
-
-        # add the bias term. Since the bias is a vector (1D array), we first
-        # reshape it to a tensor of shape (1, n_filters, 1, 1). Each bias will
-        # thus be broadcasted across mini-batches and feature map
-        # width & height
-        self.output = activation(pooled_out + self.b.dimshuffle('x', 0, 'x', 'x'))
-        self.filter_shape=filter_shape
-        self.pool_stride=pool_stride
-        self.pool_size=pool_size
-        self.conv_stride=conv_stride
-        self.image_shape=image_shape
-        # store parameters of this layer
-        self.params = [self.W, self.b]
-
-def get_channel_shape(layer):
-    filter_shape = layer.filter_shape[2:]
-    image_shape = layer.image_shape[2:]
-    pool_size = layer.pool_size
-    pool_stride = layer.pool_stride
-    conv_stride = layer.conv_stride
-    def reduction(image,kernel,stride):
-        return (image - kernel)/stride +1
-    post_conv = (reduction(image_shape[0],filter_shape[0],conv_stride[0]) ,reduction(image_shape[1],filter_shape[1],conv_stride[1]))
-    post_pool = (reduction(post_conv[0],pool_size[0],pool_stride[0]) ,reduction(post_conv[1],pool_size[1],pool_stride[1])) 
-    return post_pool      
+rng = numpy.random.RandomState(23455)
+learning_rate=0.01
+filter_shapes=[(5,5),(5,5)]
+pool_size=[(2,2),(2,2)]
+pool_stride=[(2,2),(2,2)]
+nkerns=[16,32]
+n_epochs=100
+nb_channels=3
+image_size=[128,64]   
 ################################################################################################
 ################################################################################################
 ################################################################################################
 ################################################################################################
-'''
-batch_size = 100
-dataset = '/home/local/USHERBROOKE/havm2701/git.repos/Deep_VAMP/theano/mnist.pkl.gz'
-datasets = load_data(dataset)
-train_set_x, train_set_y = datasets[0]
-valid_set_x, valid_set_y = datasets[1]
-test_set_x, test_set_y = datasets[2]
-'''
+
 path_testset = '/home/local/USHERBROOKE/havm2701/data/DBFrames'
 batch_size = 100
-train = VAMP(start=0,stop=10000,image_resize=[28,28],toronto_prepro=True)
-valid = VAMP(start=10000,stop=12000,image_resize=[28,28],toronto_prepro=True)
+train = VAMP(start=0,stop=10000,image_resize=image_size,toronto_prepro=True)
+valid = VAMP(start=10000,stop=12000,image_resize=image_size,toronto_prepro=True)
 train_x = train.X 
 train_y = np.argmax(train.y,axis=1)
 valid_x = valid.X
@@ -412,13 +52,7 @@ n_valid_batches = valid_set_x.get_value(borrow=True).shape[0] / batch_size
 n_test_batches = test_set_x.get_value(borrow=True).shape[0] / batch_size
 
 
-rng = numpy.random.RandomState(23455)
-learning_rate=0.01
-filter_shapes=[(5,5),(5,5)]
-pool_size=[(2,2),(2,2)]
-pool_stride=[(2,2),(2,2)]
-nkerns=[16,32]
-n_epochs=100
+
 x = T.matrix('x')   # the data is presented as rasterized images
 y = T.ivector('y')  # the labels are presented as 1D vector of
 index = T.lscalar()  # index to a [mini]batch                        # [int] labels
@@ -431,15 +65,14 @@ print '... building the model'
 # Reshape matrix of rasterized images of shape (batch_size, 28 * 28)
 # to a 4D tensor, compatible with our LeNetConvPoolLayer
 # (28, 28) is the size of MNIST images.
-layer0_input = x.reshape((batch_size, 28, 28,3))
+layer0_input = x.reshape((batch_size, image_size[0], image_size[1],nb_channels))
 layer0_input = layer0_input.dimshuffle(0,3,1,2)
-nb_channels=3
-image_size=[28,28]
+
 # Construct the first convolutional pooling layer:
 # filtering reduces the image size to (28-5+1 , 28-5+1) = (24, 24)
 # maxpooling reduces this further to (24/2, 24/2) = (12, 12)
 # 4D output tensor is thus of shape (batch_size, nkerns[0], 12, 12)
-layers = [LeNetConvPoolLayer(rng=rng, input=layer0_input, 
+layers = [models2.LeNetConvPoolLayer(rng=rng, input=layer0_input, 
                                          filter_shape=(nkerns[0],nb_channels,filter_shapes[0][0],filter_shapes[0][1]), 
                                          image_shape=(batch_size,nb_channels,image_size[0],image_size[1]), 
                                          activation=T.tanh, 
@@ -455,8 +88,8 @@ layers = [LeNetConvPoolLayer(rng=rng, input=layer0_input,
 for h_id in range(1,len(nkerns)):
 
     nb_channels_h = layers[-1].filter_shape[0]
-    featuremap_shape = get_channel_shape(layers[-1])
-    layers += [LeNetConvPoolLayer(rng=rng, input=layers[-1].output, 
+    featuremap_shape = models2.get_channel_shape(layers[-1])
+    layers += [models2.LeNetConvPoolLayer(rng=rng, input=layers[-1].output, 
                                           filter_shape=(nkerns[h_id],nkerns[h_id-1],filter_shapes[h_id][0],filter_shapes[h_id][1]), 
                                           image_shape=(batch_size,nkerns[h_id-1],featuremap_shape[0],featuremap_shape[1]), 
                                           activation=T.tanh, 
@@ -470,18 +103,18 @@ for h_id in range(1,len(nkerns)):
 # This will generate a matrix of shape (batch_size, nkerns[1] * 4 * 4),
 # or (500, 50 * 4 * 4) = (500, 800) with the default values.
 #layer2_input = layers[-1].output.flatten(2)
-
+last_conv_fm_shape = models2.get_channel_shape(layers[-1])
 # construct a fully-connected sigmoidal layer
-layers += [HiddenLayer(
+layers += [models2.HiddenLayer(
     rng,
     input=layers[-1].output.flatten(2),
-    n_in=nkerns[1] * 4 * 4,
+    n_in=nkerns[-1] * last_conv_fm_shape[0] * last_conv_fm_shape[1],
     n_out=500,
     activation=T.tanh
 )]
 
 # classify the values of the fully-connected sigmoidal layer
-layers += [LogisticRegression(input=layers[-1].output, n_in=500, n_out=10)]
+layers += [models2.LogisticRegression(input=layers[-1].output, n_in=500, n_out=10)]
 
 # the cost we minimize during training is the NLL of the model
 cost = layers[-1].negative_log_likelihood(y)
@@ -512,8 +145,7 @@ validate_model = theano.function(
 params = layers[0].params
 for idx in range(1,len(layers)):   
     params += layers[idx].params
-import pdb
-pdb.set_trace()
+
 # create a list of gradients for all model parameters
 grads = T.grad(cost, params)
 
